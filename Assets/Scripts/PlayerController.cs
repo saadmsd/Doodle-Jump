750f962e8b8742fc632a14b116ffdc308bc53fc2
saadmsd <saadmousstaid@gmail.com>
2024-12-02 02:27:58 +0100

    add anim tire + projectile

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public Rigidbody2D rb;
-
+    public GameObject projectilePrefab;
     private float moveX;
-
+    private bool IsShooting;
+    private Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveX = Input.GetAxis("Horizontal") * moveSpeed;
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetTrigger("Shoot");
+            Vector2 position = transform.position;
+            position.y += 0.5f;
+            GameObject projectile = Instantiate(projectilePrefab, position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+            Destroy(projectile, 1);
+        }
     }
 
     private void FixedUpdate()
@@ -27,7 +38,12 @@ public class PlayerController : MonoBehaviour
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
-
+        // if (IsShooting)
+        // {
+        //     animator.SetTrigger("Shoot");
+        //     GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        //     projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+        // }
         // inverser le sprite du joueur si on va à gauche
         if (moveX < 0)
         {
